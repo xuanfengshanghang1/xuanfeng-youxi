fa2299a766bb59c65b39257ccdd0a06ebfd4c997
xuanfengshanghang1 <370835291@qq.com>
2026-06-20 17:10:01 +0800

    reset: remove get_sidebar.js

--- get_sidebar.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var items = document.querySelectorAll('.qcs-sidebar a, .qcs-sidebar li, [class*=sidebar] a, [class*=menu] a');
-  var texts = [];
-  for (var i = 0; i < items.length; i++) {
-    texts.push(items[i].textContent.trim());
-  }
-  return texts.join(' | ');
-})()
